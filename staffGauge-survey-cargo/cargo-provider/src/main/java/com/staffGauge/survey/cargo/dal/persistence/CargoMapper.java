0c27c4e8bc3278e418f298452af414ff501499ef
throws-exceptions <1163107972@qq.com>
2020-02-10 10:15:24 +0800

    2020.2.10

---
+++ staffGauge-survey-cargo/cargo-provider/src/main/java/com/staffGauge/survey/cargo/dal/persistence/CargoMapper.java
@@ -0,0 +1,7 @@
+package com.staffGauge.survey.cargo.dal.persistence;
+
+import com.staffGauge.survey.cargo.dao.Cargo;
+import com.staffGauge.survey.cargo.tkmapper.TKMapper;
+
+public interface CargoMapper extends TKMapper<Cargo> {
+}
